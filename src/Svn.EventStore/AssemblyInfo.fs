f824e3e3a11c07eb0244fa4a6bb8da623b9b3d4f
Nikita Ignatov <hi@nikitaignatov.com>
2016-11-08 21:10:25 +0100

    added a project for importing data into eventstore

---
+++ src/Svn.EventStore/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace Svn.EventStore.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Svn.EventStore")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Svn.EventStore")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("c9fa4e74-3651-4853-a80f-4a67441f7613")>]
+[<assembly: AssemblyVersion("0.0.1.*")>]
+do
+    ()
---
+++ src/Svn.EventStore/Program.fs
@@ -0,0 +1,8 @@
+﻿namespace Svn.EventStore
+module Program = 
+    open Svn
+    open Svn.Model
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" (Configuration.svn_download_folder())
+        Setup.create_if_missing (Configuration.svn_download_folder())
